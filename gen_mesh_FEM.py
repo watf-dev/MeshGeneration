8b8a66442488e5eea86bf043a6e06d0343682181
watf-dev <wataru.fukuda.530@gmail.com>
2024-12-09 19:57:49 +0100

    init

---
+++ gen_mesh_FEM.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3.8
+# Created: Oct, 25, 2024 19:04:41 by Wataru Fukuda
+
+import os
+import watfio
+from watfmesh.MeshGeneration import MeshGeneration
+
+# DEBUG = True
+DEBUG = False
+
+
+def main():
+  import argparse
+  parser = argparse.ArgumentParser(description="""
+  generate FEM mesh
+""")
+  parser.add_argument("-o","--outdir", default="MESH_FEM", help="output")
+  parser.add_argument("-e","--nes", type=int, action="append")
+  parser.add_argument("-s","--space", nargs=2, type=float, action="append")
+  parser.add_argument("-n","--nsd", type=int)
+  parser.add_argument("-p","--order", type=int, action="append")
+  (options, args) = parser.parse_known_args()
+  space = options.space
+  nes = options.nes
+  nsd = options.nsd
+  orders = options.order
+
+  mesh = MeshGeneration(space, nes, orders, nsd)
+  nn = mesh.genNN()
+  ne = mesh.genNE()
+  nen = mesh.genNEN()
+  npd = mesh.genNPD()
+  mxyz = mesh.genMXYZ()
+  mien = mesh.genMIEN()
+  mrng = mesh.genMRNG()
+
+  data = watfio.ConfigData("FEM")
+  data.setEndian("big")
+  data.set("nn", nn)
+  data.set("ne", ne)
+  data.set("nen", nen)
+  data.set("nsd", nsd)
+  data.set("npd", npd)
+  data.setHeavyData("xyz", mxyz)
+  data.setHeavyData("ien", mien)
+  data.setHeavyData("rng", mrng)
+  filename = os.path.join(options.outdir, "mesh.cfg")
+  data.writeAll(filename)
+
+
+if __name__ == "__main__":
+  main()
+
---
+++ run_2d.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# Created: Oct, 27, 2024 21:07:23 by Wataru Fukuda
+
+ELE_X=$1
+ELE_Y=$2
+
+./gen_mesh_FEM.py -o MESH_FEM_2D -e $ELE_X -e $ELE_Y -s 0 $ELE_X -s 0 $ELE_Y -p 1 -p 1 -n 2
+
